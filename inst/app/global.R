d2810a48d554a15e67f41f0f92f2438ac3d71e0b
ikkyle <iankyle1@gmail.com>
2015-02-20 10:53:51 -0800

    initial commit

---
+++ inst/app/global.R
@@ -0,0 +1,66 @@
+# create strings here for LaTeX header and body
+source('plot_effects.R')
+
+strip.args <- function(x){
+  # x is a call that needs to be striped of arguments
+  x <- as.list(x)
+  aov.calls <- c('formula', 'data')
+  glm.calls <- c('formula', 'family', 'data')
+  t.test.calls <- c('formula', 'data')
+  
+  the.call <- deparse(x[[1]])
+  
+  x <- switch(the.call, 
+              aov=x[c(1, which(names(x) %in% aov.calls))],
+              glm=x[c(1, which(names(x) %in% glm.calls))],
+              t.test=x[c(1, which(names(x) %in% t.test.calls))])
+  
+  x <- as.call(x)
+  return(x)
+}
+
+is.numeric2 <- function(x){
+  is.numeric(x) & length(unique(x)) > 5
+}
+  
+
+p.val <- function(x, sig.indicator=FALSE, pass.chars=FALSE){
+  if(pass.chars==FALSE){
+    x <- as.numeric(x)
+    p.val <- ifelse(x >= .05, paste0('p=', format(round(x, 2), nsmall=2)), 
+                    ifelse(x < .05 & x >= .01, 'p<.05', 
+                           ifelse(x < .01 & x >= .001, 'p<.01', 
+                                  ifelse(x< .001, 'p<.001', NA))))
+  }
+  else{  
+    p.val <- ifelse(is.na(as.numeric(x)), x, 
+                    ifelse(as.numeric(x) >= .05, paste0('p=', format(round(as.numeric(x), 2), nsmall=2)), 
+                           ifelse(as.numeric(x) < .05 & as.numeric(x) >= .01, 'p<.05', 
+                                  ifelse(as.numeric(x) < .01 & as.numeric(x) >= .001, 'p<.01', 
+                                         ifelse(as.numeric(x)< .001, 'p<.001', NA)))))
+  }
+  if(sig.indicator==TRUE){
+    p.val <- ifelse(p.val=='p<.05', paste0(p.val, '*'),
+                    ifelse(p.val=='p<.01', paste0(p.val, '**'),
+                           ifelse(p.val=='p<.001', paste0(p.val, '***'),
+                                  p.val))) }
+  return(gsub('0\\.', '.', as.character(p.val)))
+}
+
+xtable.htest <- function(x){
+  tstat <- round(x$statistic, 2)
+  df    <- round(x$parameter, 2)
+  pval  <- p.val(x$p.value)
+  estimate <- round(x$estimate,2)
+  grps <- paste0(attributes(estimate)$names, ' estimate')
+  caption=paste(x$method, 'for', x$data.name)
+  
+  tbl <- data.frame('t-value'=c(tstat),
+                    df=c(df),
+                    estimate1 = estimate[1],
+                    estimate2 = estimate[2],
+                    'p-value'=pval)
+  names(tbl)[c(1,3:5)] <- c('t-value', gsub('mean in group ','', grps), 'p-value')
+  
+  xtable(tbl, caption=caption)
+}
---
+++ inst/app/server.R
@@ -0,0 +1,358 @@
+library(knitr)     # dynamic reports
+library(lsmeans)   # analysis intrepretation
+library(effects)   # for plotting
+library(agricolae) # for datasets 
+library(shiny)     # the web app javascript engine
+library(shinyAce)  # for displaying R code - pull from github
+library(lattice)   # for plotting
+library(gridExtra) # for arranging plots
+library(shinyBS)   # http://spark.rstudio.com/johnharrison/shinyBS-Demo/
+# devtools::install_github("ebailey78/shinyBS", ref = "shinyBS3")
+
+shinyServer(function(input, output, session){
+
+  output$debug <- renderText({})
+  
+##### server side element for reading in data ####################################
+  datp <- reactive({
+    if((is.null(input$data_file) || length(input$data_file)==0) && !input$use_sample_data){return(NULL)
+    } else {
+      if(length(input$data_file) > 0 && !input$use_sample_data){
+        dat <- call('read.csv', 
+                    file=input$data_file$datapath)
+      } else {
+#         dat <- parse(text=input$sample_data_buttons)
+        dat <- as.name(input$sample_data_buttons)
+      }
+    }
+    return(dat)
+  })
+
+##### assign data to "dat" by evaluating the call to "read.csv" or loading the data
+  dat <- reactive({
+      eval(call('data', input$sample_data_buttons, 
+                package='agricolae',
+                envir = environment()))
+    dat <- eval(datp(), envir=environment())
+    if(input$use_sample_data){ dat$A <- NULL }
+    return(dat)
+    })
+
+
+##### create the code needed to read in data ##########
+  read.expr <- reactive({
+    if(!input$use_sample_data){
+      return(deparse(datp()))
+    } else {
+      l <- 'library(agricolae)  # load "agricolae" package for the sample data'
+      l <- paste0(l, '\ndata("',input$sample_data_buttons,  '")')
+      l <- paste0(l, '\n', 'my_data <- ', input$sample_data_buttons)
+    }
+    })
+
+##### display the data on the "upload data" tab ################
+  output$data_table <- renderDataTable({dat()})
+  
+
+##### UI element for selecting the desired analysis ############  
+  output$select_analysis <- renderUI({
+    if(is.null(dat())){
+      h4('please upload data first')
+    } else {
+    selectInput('analysis', 
+                'Select your statistical model',
+                choices=c('t-test'='t.test', 
+                          'ANOVA'='aov',
+                          'Linear or Generalized Linear Model'='glm'),
+                selected=NULL)
+    }
+  })
+ 
+##### UI element for selecting DV ##################################
+  output$select_dv <- renderUI({
+    if(length(input$analysis)==0){return(NULL)}
+    selectInput('dv',
+                'Select your dependent variable',
+                choices=names(dat()),
+                selected=NULL)
+  })
+  
+##### UI element for selecting IV #################################
+  output$select_iv <- renderUI({
+    if(length(input$dv)==0){return(NULL)}
+    selectInput('iv', 
+                'Select your independent variable(s)',
+                choices=names(dat2())[!names(dat2()) %in% input$dv],
+                multiple=ifelse(input$analysis=='t.test', FALSE, TRUE))
+  })
+
+##### UI element for selecting the DV type ##########################
+  output$select_dv_type <- renderUI({
+    if(length(input$dv)==0 || input$analysis != 'glm'){return(NULL)}
+    selectInput('dv_type',
+                'What type of data is your dependent variable?',
+                choices=c('continuous'='gaussian',
+                          'dichotomous (binary)'='binomial',
+                          'count' = 'poisson'),
+                selected='gaussian')
+  })
+
+##### select variable types #####################################
+
+output$var_types_select <- renderUI({
+  
+  if(is.null(dat())){
+    return(NULL)
+  }
+  
+  class.recode <- c('character'='factor', 
+                    'factor'='factor',
+                    'logical'='factor',
+                    'numeric'='numeric',
+                    'integer'='numeric')
+  btns <- list()
+  
+  for(i in 1:ncol(dat())){
+    clss <- class(dat()[,i])
+    clss <- class.recode[clss]
+    
+    btns[[i]] <- radioButtons(inputId=paste0(names(dat())[i], '_recode'),
+                             label=names(dat())[i],
+                             choices=c('Numeric'='numeric', 'Grouping'='factor'),
+                             selected=clss, 
+                             inline=TRUE)
+  } 
+  return(btns)
+})
+
+######## convert variables to their respective types ############
+
+dat2 <- reactive({
+  dat2 <- dat() 
+  for(i in 1:ncol(dat2)){
+    var_type <- input[[paste0(names(dat2)[i], '_recode')]]
+    dat2[,i] <- eval(call(paste0('as.', var_type),
+                          dat2[,i]))
+  }
+  return(dat2)
+})
+
+
+
+##### select the interactions  ################################
+# TukeyHSD or multcomp::glht
+# output$ <- renderUI({
+#   if(input$analysis == 't.test'){return(NULL)}
+#   seletInput('')
+#   })
+
+
+
+
+##### create the formula for analysis ##########################
+  fmla <- reactive({
+    paste0(input$dv, ' ~ ', 
+           paste0(input$iv, collapse=' + '))
+    })
+  
+##### run the analysis, assign to reactive object "fit" ##############
+  fitp <- reactive({
+    input$run_analysis
+    isolate({
+      if(!is.character(input$analysis)){
+        return(NULL)
+      } 
+      fit <- call(input$analysis, 
+                  formula=as.formula(fmla()),
+                  family=input$dv_type,
+                  data=as.name('my_data'))
+      fit <- strip.args(fit)
+    })
+    return(fit)
+    })
+
+fit <- reactive({
+  input$run_analysis
+    isolate({
+      my_data <- dat2()
+      x <- eval(fitp())
+    })
+  return(x)
+  })
+
+fit.expr <- reactive({
+  input$run_analysis
+  isolate({
+    x <- deparse(fitp(), width.cutoff=500L)
+  })
+  return(x)
+})
+
+##### the fit summary for analysis ###################################
+### print anova table for all linear models
+### the interpretaion tab will show post-hoc tests
+  output$fit_summary <- renderPrint({
+    analysis <- isolate(input$analysis)
+    input$run_analysis
+    if(analysis=='t.test'){
+      sum <- isolate(fit())
+    } else {
+      sum <- isolate(anova(fit(), 
+                           test=ifelse((input$analysis=='glm' & 
+                                          input$dv_type=='gaussian') |
+                                          input$analysis=='aov',
+                                       'F', 'LRT')))
+    }
+    return(sum)
+  })
+
+##### UI element for fit_summary object ####################################
+  output$fit_output <- renderUI({
+    if(is.null(input$run_analysis) || input$run_analysis==0){return(NULL)}
+      verbatimTextOutput('fit_summary')
+  })
+  
+##### UI element - the "run analysis" button ################################
+  output$action_button <- renderUI({
+    actionButton('run_analysis', 'Run analysis')
+  })
+  
+##### the code for reading in the data ###################################
+  read_code <- reactive({
+    if(length(input$data_file)==0 && !input$use_sample_data){return(NULL)}
+    if(!input$use_sample_data){
+    filestr <- gsub('(?<=file \\= ").*(?="\\))', 
+                    input$data_file$name, perl=TRUE,
+                    read.expr())
+    filestr <- paste0('my_data <- ', filestr)
+    } else {
+      filestr <- read.expr()
+    }
+    return(filestr)
+  })
+ 
+##### UI update for the reading in code display ##########################
+  observe({
+    updateAceEditor(session, 'code_used_read', value=read_code(),
+                    readOnly=TRUE, wordWrap=TRUE)
+  })
+  
+##### return the code used to run the model ##############################
+ mcode <- reactive({
+   if(is.null(input$analysis) || input$run_analysis==0){return(NULL)}
+    mcode <- paste0('model.fit <- ', fit.expr())
+   
+   mcode <- paste(mcode, sep='', collapse='')
+   mcode <- gsub('(\\, )', ',\n\t\t\t', mcode)
+   
+   
+   analysis.name <- c('aov'='ANOVA', 'glm'='(generalized) linear model',
+                      't.test'='t-test')[input$analysis]
+   
+   factor.ind <- which(sapply(dat2(), is.factor))
+    if(length(factor.ind)>0){
+      factor.name <- names(dat2()[factor.ind])
+      
+      fcode <- paste0('my_data$', factor.name, ' <- as.factor(my_data$', 
+                      factor.name, ')', collapse='\n')
+      mcode <- paste0('# convert categorical variables to factors\n', 
+                      fcode, '\n\n# run ', analysis.name,'\n',
+                      mcode)
+    }
+   return(mcode)
+  })
+  
+##### update the editor to display code used ##############################
+  observe({
+    input$run_analysis
+    updateAceEditor(session, 'code_used_model', value=isolate(mcode()),
+                    readOnly=TRUE, wordWrap=TRUE)
+  })
+  
+##### generate plots from the effects package ###############################
+ ef <- reactive({
+    input$update_plots
+    if(input$analysis=='t.test'){
+      return(NULL)
+    }
+   ef <- vector('list', length(input$iv))
+   names(ef) <- input$iv
+   assign('my_data', dat2(), env=.GlobalEnv)
+   assign('.fit', fit(), env=.GlobalEnv)
+   for(i in input$iv){
+     ef[[i]] <- Effect(i, .fit)
+   }
+   remove('.fit', env=.GlobalEnv)
+   remove('my_data', envir=.GlobalEnv)
+   return(ef)
+ })
+ 
+##### server element - generate the plots ######################################
+ output$effect_plots <- renderPlot({
+   if(input$analysis=='t.test'){
+     return(NULL)
+   }
+   ps <- lapply(ef(), plot, ci.style='bars')
+   for(i in seq_along(ps)){
+     class(ps[[i]]) <- 'trellis'
+   }
+   p <- do.call(arrangeGrob, 
+                c(ps, ncol=2))
+   return(p)
+ })
+ 
+##### UI element - print the plots ##########################################
+ output$plots <- renderUI({
+   if(is.null(fit())){
+     return(NULL)
+   }
+   if(input$analysis=='t.test'){
+     return(h4('Effect plots not available with t-tests'))
+   }
+   h <- (length(ef()) + 1) %/% 2
+   plotOutput('effect_plots', 
+              height=h * 450)
+ })
+  
+
+##### Histograms #############################################################
+output$histograms <- renderPlot({
+  varinds <- which(sapply(dat2(), is.numeric2))
+  par(mfrow=c((length(varinds) + 1) %/% 2, 2))
+  for(i in names(dat2())[varinds]){
+    hist(dat2()[,i], main=i, xlab='')
+  }
+  })
+
+
+output$hists <- renderUI({
+  if(is.null(dat2())){return(NULL)}
+  h <- (length(which(sapply(dat2(), is.numeric2))) + 1) %/% 2
+  plotOutput('histograms',
+             height=h *450)
+})
+
+##### Download report #####################################################
+output$download_report <- downloadHandler(
+  filename = function() {
+    paste0(input$analysis, '_analysis_report_', Sys.Date(),'.html')
+  },
+  content = function(file) {
+    src <- normalizePath('report.Rmd')
+    
+    file.copy(src, 'report.Rmd')
+    
+    out <- knit2html('report.Rmd', 
+                     output=paste0(input$analysis, '_analysis_report_', Sys.Date(),'.html'))
+    file.rename(out, file)
+  }
+)
+
+
+ 
+##### stop the session after it's over #########################################
+  session$onSessionEnded(function() { 
+    stopApp()
+  })
+  
+})
