6cbf09643738a1389c7332b7d2032012e82174ab
ikkyle <iankyle1@gmail.com>
2015-02-27 12:53:58 -0800

    initial commit

---
+++ R/initialize_AIP.R
@@ -0,0 +1,43 @@
+if(!require(devtools)){
+  install.packages('devtools')
+  library(devtools)
+}
+
+if(!require(shiny)){
+  install.packages('shiny')
+}
+
+if(!require(shinyAce)){
+  install.packages('shinyAce')
+}
+
+if(!require(knitr)){
+  install.packages('knitr')
+}
+
+if(!require(gridExtra)){
+  install.packages('gridExtra')
+}
+
+if(!require(agricolae)){
+  install.packages('agricolae')
+}
+
+if(!require(lattice)){
+  install.packages('lattice')
+}
+
+if(!require(effects)){
+  install.packages('effects')
+}
+
+if(!require(lsmeans)){
+  install.packages('lsmeans')
+}
+
+if(!require(shinyBS)){
+  install_githib("ebailey78/shinyBS", ref = "shinyBS3")
+}
+
+
+
