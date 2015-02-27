7833051c8a645089a60b455bdce68a3c9bd98ee7
ikkyle <iankyle1@gmail.com>
2015-02-27 13:17:07 -0800

    add repo, shinyACE from github

--- R/initialize_AIP.R
+++ R/initialize_AIP.R
@@ -1,42 +1,49 @@
-if(!require(devtools)){
-  install.packages('devtools')
+initialize_AIP <- function(){
+
+if(!require(devtools, quietly=TRUE)){
+  install.packages('devtools', repos="http://cran.rstudio.com/")
   library(devtools)
 }
 
-if(!require(shiny)){
-  install.packages('shiny')
+if(!require(shiny, quietly=TRUE)){
+  install.packages('shiny', repos="http://cran.rstudio.com/")
 }
 
-if(!require(shinyAce)){
-  install.packages('shinyAce')
+if(!require(knitr, quietly=TRUE)){
+  install.packages('knitr', repos="http://cran.rstudio.com/")
 }
 
-if(!require(knitr)){
-  install.packages('knitr')
+if(!require(gridExtra, quietly=TRUE)){
+  install.packages('gridExtra', repos="http://cran.rstudio.com/")
 }
 
-if(!require(gridExtra)){
-  install.packages('gridExtra')
+if(!require(agricolae, quietly=TRUE)){
+  install.packages('agricolae', repos="http://cran.rstudio.com/")
 }
 
-if(!require(agricolae)){
-  install.packages('agricolae')
+if(!require(lattice, quietly=TRUE)){
+  install.packages('lattice', repos="http://cran.rstudio.com/")
 }
 
-if(!require(lattice)){
-  install.packages('lattice')
+if(!require(effects, quietly=TRUE)){
+  install.packages('effects', repos="http://cran.rstudio.com/")
 }
 
-if(!require(effects)){
-  install.packages('effects')
+if(!require(lsmeans, quietly=TRUE)){
+  install.packages('lsmeans', repos="http://cran.rstudio.com/")
 }
 
-if(!require(lsmeans)){
-  install.packages('lsmeans')
+if(!require(xtable, quietly=TRUE)){
+  install.packages('xtable', repos="http://cran.rstudio.com/")
 }
 
-if(!require(shinyBS)){
-  install_githib("ebailey78/shinyBS", ref = "shinyBS3")
+if(!require(shinyAce, quietly=TRUE)){
+  install_github('trestletech/shinyAce')
+}
+
+if(!require(shinyBS, quietly=TRUE)){
+  install_github("ebailey78/shinyBS", ref = "shinyBS3")
+}
 }
 
 
