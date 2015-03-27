ebd8c772ddc31bb318ac840ae5503d1f14d97958
ikkyle <iankyle1@gmail.com>
2015-03-27 11:35:49 -0700

    comment out install for readxl.

--- R/initialize_AIP.R
+++ R/initialize_AIP.R
@@ -1,49 +1,51 @@
 initialize_AIP <- function(){
-
-if(!require(devtools, quietly=TRUE)){
-  install.packages('devtools', repos="http://cran.rstudio.com/")
-  library(devtools)
-}
-
-if(!require(shiny, quietly=TRUE)){
-  install.packages('shiny', repos="http://cran.rstudio.com/")
-}
-
-if(!require(knitr, quietly=TRUE)){
-  install.packages('knitr', repos="http://cran.rstudio.com/")
-}
-
-if(!require(gridExtra, quietly=TRUE)){
-  install.packages('gridExtra', repos="http://cran.rstudio.com/")
-}
-
-if(!require(agricolae, quietly=TRUE)){
-  install.packages('agricolae', repos="http://cran.rstudio.com/")
-}
-
-if(!require(lattice, quietly=TRUE)){
-  install.packages('lattice', repos="http://cran.rstudio.com/")
-}
-
-if(!require(effects, quietly=TRUE)){
-  install.packages('effects', repos="http://cran.rstudio.com/")
-}
-
-if(!require(lsmeans, quietly=TRUE)){
-  install.packages('lsmeans', repos="http://cran.rstudio.com/")
-}
-
-if(!require(xtable, quietly=TRUE)){
-  install.packages('xtable', repos="http://cran.rstudio.com/")
-}
-
-if(!require(shinyAce, quietly=TRUE)){
-  install_github('trestletech/shinyAce')
-}
-
-if(!require(shinyBS, quietly=TRUE)){
-  install_github("ebailey78/shinyBS", ref = "shinyBS3")
-}
+  
+  if(!require(devtools, quietly=TRUE)){
+    install.packages('devtools', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(shiny, quietly=TRUE)){
+    install.packages('shiny', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(knitr, quietly=TRUE)){
+    install.packages('knitr', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(gridExtra, quietly=TRUE)){
+    install.packages('gridExtra', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(agricolae, quietly=TRUE)){
+    install.packages('agricolae', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(lattice, quietly=TRUE)){
+    install.packages('lattice', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(effects, quietly=TRUE)){
+    install.packages('effects', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(lsmeans, quietly=TRUE)){
+    install.packages('lsmeans', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(xtable, quietly=TRUE)){
+    install.packages('xtable', repos="http://cran.rstudio.com/")
+  }
+  
+  if(!require(shinyAce, quietly=TRUE)){
+    devtools::install_github('trestletech/shinyAce')
+  }
+  
+  if(!require(shinyBS, quietly=TRUE)){
+    devtools::install_github("ebailey78/shinyBS", ref = "shinyBS3")
+  }
+#   if(!require(readxl, quietly=TRUE)){
+#     install.packages("readxl", repos="http://cran.rstudio.com/")
+#   }
 }
 
 
