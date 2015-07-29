24121696f1b8491f07c537e6ac606a80703b5c64
Jason K. Moore <moorepants@gmail.com>
2015-07-29 15:14:57 -0700

    Tidied it up and tried to add Levene's Test but failed.

--- examples/rcbd_two_var.R
+++ examples/rcbd_two_var.R
@@ -3,10 +3,20 @@
 # Complete Block Design. It is based on the example given on page 36 of the
 # agricolae tutorial[1].
 #
+# To run the script with full output:
+#
+# source('rcbd_two_var.R', print.eval = TRUE)
+#
 # [1] https://cran.r-project.org/web/packages/agricolae/vignettes/tutorial.pdf
+#
+# NOTE : All lines shown to the user in the app are surrounded by this
+# separator: #-----------------------------#
 
-library('agricolae')
+#-----------------------------------------------------------------------------#
+library('agricolae')  # for LSD.test()
+library('car')  # for levenTest()
 library('effects')
+#-----------------------------------------------------------------------------#
 
 sep <- function(n){
   # This function simply prints a line of "=" to the screen as a separator and
@@ -24,48 +34,80 @@ yield <- c(6, 7, 9, 13, 16, 20, 8, 8, 9,
 block <- gl(4, 9)
 clone <- rep(gl(3, 3, labels = c('c1', 'c2', 'c3')), 4)
 nitrogen <- rep(gl(3, 1, labels = c('n1', 'n2', 'n3')), 12)
-data <- data.frame(block, clone, nitrogen, yield)
+my.data <- data.frame(block, clone, nitrogen, yield)
+write.csv(my.data, 'rcbd_two_var.csv', quote=FALSE, row.names=FALSE)
 
-model <- aov(yield ~ block + clone + nitrogen + clone:nitrogen, data=data)
+# Load the data.
+#-----------------------------------------------------------------------------#
+my.data <- read.csv('rcbd_two_var.csv')
+my.data$block <- as.factor(my.data$block)
+#-----------------------------------------------------------------------------#
+
+# Construct the model.
+#-----------------------------------------------------------------------------#
+model <- aov(yield ~ block + clone + nitrogen + clone:nitrogen, data=my.data)
+#-----------------------------------------------------------------------------#
 
 # Plot the four standard fit plots: residuals vs predicted, sqrt of residuals vs
 # fitted, Normal Q-Q plot of the residuals, residuals vs leverages.
 dev.new()
+#------------------------------------------------------------------------------#
 par(mfrow = c(2, 2), oma = c(0, 0, 2, 0))
 plot(model)
+#------------------------------------------------------------------------------#
 
 # Make sure the residuals are normal (this can also be seen in the Q-Q plot).
 cat('Shapiro-Wilk Normality Test\n')
 sep(50)
+#------------------------------------------------------------------------------#
 shapiro.test(residuals(model))
+#------------------------------------------------------------------------------#
 sep(50)
 
-# User selected alpha level.
+# Run Levene's Test.
+# TODO : This gives the error: "Model must be completely crossed formula only."
+cat("Levene's Test\n")
+sep(79)
+#------------------------------------------------------------------------------#
+#leveneTest(model)
+#------------------------------------------------------------------------------#
+sep(79)
+
+# The user will select the alpha level from the app.
+#------------------------------------------------------------------------------#
 alpha <- 0.05
+#------------------------------------------------------------------------------#
 
 # Print the ANOVA table of the fit. The user should will have to note the
 # significant factors. In this case the single virus factor is significant.
 cat('ANOVA Table\n')
 sep(50)
+#------------------------------------------------------------------------------#
 anova(model)
+#------------------------------------------------------------------------------#
 sep(50)
 
 # Show the confidence intervals for the intercept and each virus level.
 cat('Confidence Intervals\n')
 sep(50)
+#------------------------------------------------------------------------------#
 confint(model, level=1.0 - alpha)
+#------------------------------------------------------------------------------#
 sep(50)
 
 # Plot the mean yield with respect to each virus level.
 # TODO : Is the default plot ok here, or is something different desired?
 dev.new()
+#------------------------------------------------------------------------------#
 plot(allEffects(model))
+#------------------------------------------------------------------------------#
 
 # The ANOVA table shows that each variable and the interaction are significant,
 # so we then see which levels of both clone and nitrogen are significant with
 # respect to each other using least significant difference.
-# TODO : It isn't clear to me why the "block" variable wouldn't be checked here.
 cat('Least Significant Difference\n')
 sep(50)
+#------------------------------------------------------------------------------#
 LSD.test(model, c('clone', 'nitrogen'), alpha=alpha, console=TRUE)
+#------------------------------------------------------------------------------#
 sep(50)
