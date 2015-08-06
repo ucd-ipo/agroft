a2675e1fda6ebfd3d9017d02cd41a6ec12aab999
msimmond <mbsimmonds@ucdavis.edu>
2015-08-06 15:21:00 -0700

    create bar graph in ggplot
    This is the type and characteristics of the bar graph that should be available for all scenarios.

--- examples/crd_one_var.R
+++ examples/crd_one_var.R
@@ -95,3 +95,43 @@ sep(79)
 LSD.test(model, "virus", console=TRUE)
 #-----------------------------------------------------------------------------#
 sep(79)
+
+##Create Post-hoc Bar Graph with LSD letter labels##
+#Save lsd summary
+lsd <- LSD.test(model, "virus", group = TRUE)
+#Save each component in temporary df to use in creating bar graph
+lsd.letters <- as.character(lsd$groups[,3])
+lsd.trt.means <- (lsd$groups[,2])
+lsd.trt.names <- (lsd$groups[,1])
+table1 <- data.frame(lsd.trt.names,lsd.trt.means,lsd.letters)
+#Calc standard errors for error bars in graph, and save in temporary df 
+Data2 <- data.frame(summarySE(data=my.data, 
+                  "yield", 
+                  groupvars="virus", 
+                  conf.interval = 0.95))
+#merge into one df so we have LSD letters matching with means and standard errors
+merged_table <- merge(Data2,table1,by.x='virus',by.y='lsd.trt.names')
+
+#create bar graph
+library(ggplot2) 
+ggplot(merged_table, 
+       aes(x = virus, y = lsd.trt.means, 
+           ymax=50, ymin=0.0))  + #note scale depends on dataset
+  geom_bar(stat="identity", fill="gray50",
+           colour = "black", width = 0.7)  +
+  geom_errorbar(aes(ymax=lsd.trt.means+se, ymin=lsd.trt.means-se), 
+                width=0.0, size=0.5, color="black")  +
+ geom_text(aes(label=lsd.letters,
+               y = lsd.trt.means + se/1.8, vjust=-2.5))
+  labs(x = "Virus",   #should be customizable
+       y = "Yield")  +  #should be customizable
+  theme_bw()  +
+  theme(panel.grid.major.x = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey80"),
+        plot.title = element_text(size = rel(1.5), 
+                                  face = "bold", vjust = 1.5),
+        axis.title = element_text(face = "bold"),
+        axis.title.y = element_text(vjust= 1.8),
+        axis.title.x = element_text(vjust= -0.5),
+        panel.border = element_rect(colour="black")
+  )
