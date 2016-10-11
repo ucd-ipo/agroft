8661f316883eb990d8ccf738be9e8c5d87ac7d79
Ian Kyle <iankyle1@gmail.com>
2016-10-10 21:30:31 -0500

    not working. weird.

--- R/dratinit.R
+++ R/dratinit.R
@@ -1,8 +1,7 @@
-library(drat)
+# library(drat)
 
-pkg <- '../agroft_0.2.tar.gz'
-insertPackage(pkg, '~/drat', commit='push')
-
+# pkg <- '../agroft_0.2.tar.gz'
+# insertPackage(pkg, '~/drat', commit='push')
 
 # install.packages('agroft', 
 #                  repos = c('@CRAN@', 'https://ucd-ipo.github.io/agroft'), 
