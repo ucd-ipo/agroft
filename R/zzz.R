ee8511b97cfd39aab2cda077dbcecd11b9c7705d
ikkyle <iankyle1@gmail.com>
2016-10-10 19:20:45 -0500

    wording fixes

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname){
   packageStartupMessage('You have loaded Agroft: An Agriculture Field Trial Statistics Package.
-  Start an Agroft session by typing "launch()" at the R prompt (>) and pressing enter.
+  You can start an Agroft session by typing "launch()" at the R prompt (>) and pressing enter.
   More info on how to use Agroft can be found online at: https://github.com/ucd-ipo/agroft
   Detailed materials can be found on the about tab within Agroft.\n
 Now launching Agroft...')
