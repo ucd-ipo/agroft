76281f10f427d8e60f89723560ddc7c37d225a1a
Ian Kyle <iankyle1@gmail.com>
2016-10-11 22:04:00 -0500

    edit for clairity and consistency

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,16 @@
 .onLoad <- function(libname, pkgname){
-  packageStartupMessage('You have loaded Agroft: An Agriculture Field Trial Statistics Package.
-  You can start an Agroft session by typing "launch()" at the R prompt (>) and pressing enter.
-  More info on how to use Agroft can be found online at: https://github.com/ucd-ipo/agroft
-  Detailed materials can be found on the about tab within Agroft.\n
-Now launching Agroft...')
+  packageStartupMessage('You have loaded Agroft: An Agriculture Field Trial Statistics Package.\n
+You can start an Agroft session by typing "launch()" at the R prompt (>) and pressing enter.
+More info on how to use Agroft can be found online at: http://ucd-ipo.github.io/agroft \n
+To end your Agroft session, close the browser tab or window Agroft is running in.\n
+Important!\n- Agroft does not save your work between sessions.
+- Use the report feature in the app to save your progress.\n')
+
+	Sys.sleep(1.5)
+
+	cat('Now launching Agroft...')
+
   if (interactive()) {
-    launch() 
+    launch()
   }
 }
