9cac95ac76a1c1a7627be49ea126ece047079247
Ian Kyle <iankyle1@gmail.com>
2016-10-11 23:07:35 -0500

    remove cat for R CMD CHECK

--- R/zzz.R
+++ R/zzz.R
@@ -7,9 +7,8 @@ You can start an Agroft session by typing "launch()" at the R prompt (>) and pre
 More info on how to use Agroft can be found online at: http://ucd-ipo.github.io/agroft \n
 To end your Agroft session, close the browser tab or window Agroft is running in.\n
 Important!\n- Agroft does not save your work between sessions.
-- Use the report feature in the app to save your progress.\n')
-
-	cat('Now launching Agroft...')
+- Use the report feature in the app to save your progress.
+Now launching Agroft...')
 
     launch()
   }
