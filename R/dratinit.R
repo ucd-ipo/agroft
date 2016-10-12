43e55b890377a88e5a2c48347c937465169cdc2d
Ian Kyle <iankyle1@gmail.com>
2016-10-11 21:37:21 -0500

    move to gh pages branch

--- R/dratinit.R
+++
@@ -1,8 +0,0 @@
-# library(drat)
-
-# pkg <- '../agroft_0.2.tar.gz'
-# insertPackage(pkg, '~/drat', commit='push')
-
-# install.packages('agroft', 
-#                  repos = c('@CRAN@', 'https://ucd-ipo.github.io/agroft'), 
-#                  type='source')
