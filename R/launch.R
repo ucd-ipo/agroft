c1453660c95bd84a844774df3bce102005d34747
Ian Kyle <iankyle1@gmail.com>
2016-10-11 22:53:17 -0500

    protect against batch mode

--- R/launch.R
+++ R/launch.R
@@ -1,3 +1,6 @@
 launch <- function(){
+	if (!interactive())
+		stop('The Agroft package is designed for interactive use only', call.=TRUE)
+
   shiny::runApp(system.file('app', package='agroft'))
 }
